fd5201ad5dedd4c45aec529f9054e6833cf2fa3b
AlessandroMezzina <mezzina.alessandro97@gmail.com>
2024-06-01 23:59:45 +0200

    Introduzione creazione playlist

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python    
 
+# Reference:
+# https://developer.spotify.com/documentation/web-api/reference/
+# https://spotipy.readthedocs.io/en/2.24.0/
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import configparser
 
 from resources.readUtils import utils
-
-config = configparser.RawConfigParser()
-config.read('properties.properties')
-
-CLIENT_ID = config.get("Authentication","CLIENT_ID")
-CLIENT_SECRET = config.get("Authentication","CLIENT_SECRET")
-REDIRECT_URI = config.get("Authentication","REDIRECT_URI")
-
-# multiple scopes
-oAuthscope = "user-read-email,user-read-private,user-library-read,user-read-playback-state,user-modify-playback-state,user-read-currently-playing"
-
-#autenticazione
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI,scope=oAuthscope))
 
 def getCurrentSong():
     response_current_track = sp.current_user_playing_track()
@@ -47,13 +38,52 @@ def getUserLikedTracks():
         response = sp.current_user_saved_tracks(market=MARKET, limit=LIMIT, offset=OFFSET)
         likedTracks = response["items"]
         for track in likedTracks:
-            tracks.append(track["track"]["name"])
+            singleTrack = Track()
+            singleTrack.songName = track["track"]["name"]
+            singleTrack.artist = track["track"]["artists"]
+            singleTrack.id = track["track"]["id"]
+            tracks.append(singleTrack)
         if response["next"]:
             OFFSET+=50
         else:
             end=True
     return tracks
 
+def createPlaylist():
+    # ENDPOINT https://api.spotify.com/v1/users/{user_id}/playlists
+    # PARAMETRI GET PER QUESTA CHIAMATA:
+    ## user (OBBLIGATORIO)          : l'id dell'utente
+    ## name (OBBLIGATORIO)          : il nome della playlist
+    ## public (OPZIONALE)           : se è pubblica o privata
+    ## collaborative (OPZIONALE)    : se la playlist è in collaborazione
+    ## description (OPZIONALE)      : descrizione della playlist
+
+    user_id = sp.me()["id"];
+    sp.user_playlist_create(user=user_id, name="Playlist prova", description="Playlist prova")
+    return
+
+class Track:
+    songName = ""
+    artist = ""
+    id = ""
+
+
+config = configparser.RawConfigParser()
+config.read('properties.properties')
+
+CLIENT_ID = config.get("Authentication","CLIENT_ID")
+CLIENT_SECRET = config.get("Authentication","CLIENT_SECRET")
+REDIRECT_URI = config.get("Authentication","REDIRECT_URI")
+
+# multiple scopes
+# playlist-modify-public -> Necessario per interagire con le playlist
+oAuthscope = "user-read-email,user-read-private,user-library-read,user-read-playback-state,user-modify-playback-state,user-read-currently-playing, playlist-modify-public"
+
+#autenticazione
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI,scope=oAuthscope))
+
 #getCurrentSong()
 tracks = getUserLikedTracks()
 print("Hai messo 'mi piace' a " + str(len(tracks)) + " brani")
+
+#createPlaylist()
