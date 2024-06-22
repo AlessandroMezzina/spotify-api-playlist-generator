8dc71b70a28a3aa485597aec5b89bc1793471ec6
AlessandroMezzina <mezzina.alessandro97@gmail.com>
2024-06-22 15:02:45 +0200

    Riorganizzazione del codice

--- main_gui.py
+++ main_gui.py
@@ -14,6 +14,109 @@ from tkinter import ttk
 from ttkthemes import ThemedTk
 import webbrowser
 
+config = configparser.RawConfigParser()
+config.read('properties.properties')
+
+CLIENT_ID = config.get("Authentication","CLIENT_ID")
+CLIENT_SECRET = config.get("Authentication","CLIENT_SECRET")
+REDIRECT_URI = config.get("Authentication","REDIRECT_URI")
+
+#Variabili globali
+is_generating = False  # Variabile per tenere traccia dello stato di generazione della playlist
+sp = None  # Variabile globale per l'oggetto SpotifyOAuth
+
+# multiple scopes
+# playlist-modify-public -> Necessario per interagire con le playlist
+oAuthscope = "user-read-email,user-read-private,user-library-read,user-read-playback-state,user-modify-playback-state,user-read-currently-playing, playlist-modify-public, playlist-modify-private"
+
+# Classi
+# Classe per rappresentare una traccia
+class Track:
+    songName = ""
+    artist = ""
+    id = ""
+    uri = ""
+
+# Classe per la finestra principale
+class FinestraPrincipale:
+    def __init__(self, master=None):
+        # Crea la finestra principale
+        # Crea un Frame per un layout migliore
+        mainFrame = ttk.Frame(window, padding="10")
+        mainFrame.pack(fill=tk.BOTH, expand=True)
+        # Assicurati che le colonne si espandano adeguatamente
+        mainFrame.grid_columnconfigure(0, weight=1)
+        mainFrame.grid_columnconfigure(1, weight=1)
+
+
+        # Aggiungi una casella di testo per l'artista
+        artistLabel = ttk.Label(mainFrame, text="Inserisci l'artista:")
+        artistLabel.grid(row=0, column=0, pady=10, columnspan=2)  # Allinea al centro
+        self.artistEntry = ttk.Entry(mainFrame)
+        self.artistEntry.grid(row=1, column=0, padx=5, pady=10, sticky="ew", columnspan=2)
+
+        # Aggiungi un pulsante per generare la playlist
+        generateButton = ttk.Button(mainFrame, text="Genera Playlist", command=self.generatePlaylist)
+        generateButton.config(width=20)  # Imposta la larghezza di generateButton
+        generateButton.grid(row=2, column=0, pady=10)  # Occupa la colonna 0, espande da est a ovest
+        # Aggiungi il bottone "Stop" accanto a "Genera Playlist"
+        stopButton = ttk.Button(mainFrame, text="Stop", command=self.stopPlaylistGeneration)
+        stopButton.config(width=20)      # Imposta la larghezza di stopButton
+        stopButton.grid(row=2, column=1, pady=10)  # Occupa la colonna 1, espande da est a ovest
+
+
+        # Crea l'etichetta per i messaggi di stato
+        self.statusLabel = ttk.Label(mainFrame, text="In attesa della generazione...")
+        self.statusLabel.grid(row=3, column=0, columnspan=2, pady=10)  # Occupa 2 colonne
+    
+    def generatePlaylist(self):
+        global is_generating
+        is_generating = True
+
+        artist = self.artistEntry.get()
+        self.updateStatus("Generazione della playlist per l'artista: " + artist + "...")
+        tracks = getUserLikedTracks()
+        #Trova solo i brani nuovi
+        tracks = checkNewTracks(tracks)
+        tracks = filterByArtist(tracks, artist)
+
+        if tracks:
+            self.updateStatus("Sono stati scansionati nuovi " + str(len(tracks)) + " brani piaciuti")
+        else:
+            self.updateStatus("Nessun nuovo brano piaciuto è stato riconosciuto per l'artista: " + artist)
+            return
+        playlists = retrievePlaylists()
+        # Elementi utili: playlists["items"]["i"]["id"], playlists["items"]["i"]["name"]
+
+        # Spotify permette con lo stesso nome di creare anche più di una playlist. E' da evitare
+        # Quindi verifico se la playlist esiste già. Se non esiste la creo
+        playlistName = artist + "- PY"
+        createdPlaylist = checkPlaylistExists(playlistName, playlists)
+        if createdPlaylist == False:
+            # Se la playlist non esiste, la creo e aggiungo la traccia
+            createdPlaylist = createPlaylist(playlistName)
+        # Adesso sono certo che la playlist generata esista. Posso aggiungere la traccia
+        self.updateStatus("Playlist generata: " + playlistName)
+
+        for track in tracks:
+            if not is_generating:
+                return
+            # Della playlist creata mi interessa createdPlaylist["id"] per poter aggiungere nuove tracce
+            if not checkPlaylistItems(createdPlaylist["id"], track.id):
+                addItemsToPlaylist(createdPlaylist["id"], track.uri)
+        self.updateStatus("Playlist aggiornata con successo!")
+    
+    # Funzione per aggiornare il messaggio di stato
+    def updateStatus(self, message):
+        self.statusLabel.config(text=message)
+        window.update()
+    
+    def stopPlaylistGeneration(self):
+        global is_generating
+        is_generating = False
+        self.updateStatus("Generazione della playlist interrotta.")
+
+# Funzioni per interagire con Spotify
 def getCurrentSong():
     global sp
     response_current_track = sp.current_user_playing_track()
@@ -174,107 +277,6 @@ def checkPlaylistItems(playlist_id, track_id):
         
     return found
 
-class Track:
-    songName = ""
-    artist = ""
-    id = ""
-    uri = ""
-
-class FinestraPrincipale:
-    def __init__(self, master=None):
-        # Crea la finestra principale
-        # Crea un Frame per un layout migliore
-        mainFrame = ttk.Frame(window, padding="10")
-        mainFrame.pack(fill=tk.BOTH, expand=True)
-        # Assicurati che le colonne si espandano adeguatamente
-        mainFrame.grid_columnconfigure(0, weight=1)
-        mainFrame.grid_columnconfigure(1, weight=1)
-
-
-        # Aggiungi una casella di testo per l'artista
-        artistLabel = ttk.Label(mainFrame, text="Inserisci l'artista:")
-        artistLabel.grid(row=0, column=0, pady=10, columnspan=2)  # Allinea al centro
-        self.artistEntry = ttk.Entry(mainFrame)
-        self.artistEntry.grid(row=1, column=0, padx=5, pady=10, sticky="ew", columnspan=2)
-
-        # Aggiungi un pulsante per generare la playlist
-        generateButton = ttk.Button(mainFrame, text="Genera Playlist", command=self.generatePlaylist)
-        generateButton.config(width=20)  # Imposta la larghezza di generateButton
-        generateButton.grid(row=2, column=0, pady=10)  # Occupa la colonna 0, espande da est a ovest
-        # Aggiungi il bottone "Stop" accanto a "Genera Playlist"
-        stopButton = ttk.Button(mainFrame, text="Stop", command=self.stopPlaylistGeneration)
-        stopButton.config(width=20)      # Imposta la larghezza di stopButton
-        stopButton.grid(row=2, column=1, pady=10)  # Occupa la colonna 1, espande da est a ovest
-
-
-        # Crea l'etichetta per i messaggi di stato
-        self.statusLabel = ttk.Label(mainFrame, text="In attesa della generazione...")
-        self.statusLabel.grid(row=3, column=0, columnspan=2, pady=10)  # Occupa 2 colonne
-    
-    def generatePlaylist(self):
-        global is_generating
-        is_generating = True
-
-        artist = self.artistEntry.get()
-        self.updateStatus("Generazione della playlist per l'artista: " + artist + "...")
-        tracks = getUserLikedTracks()
-        #Trova solo i brani nuovi
-        tracks = checkNewTracks(tracks)
-        tracks = filterByArtist(tracks, artist)
-
-        if tracks:
-            self.updateStatus("Sono stati scansionati nuovi " + str(len(tracks)) + " brani piaciuti")
-        else:
-            self.updateStatus("Nessun nuovo brano piaciuto è stato riconosciuto per l'artista: " + artist)
-            return
-        playlists = retrievePlaylists()
-        # Elementi utili: playlists["items"]["i"]["id"], playlists["items"]["i"]["name"]
-
-        # Spotify permette con lo stesso nome di creare anche più di una playlist. E' da evitare
-        # Quindi verifico se la playlist esiste già. Se non esiste la creo
-        playlistName = artist + "- PY"
-        createdPlaylist = checkPlaylistExists(playlistName, playlists)
-        if createdPlaylist == False:
-            # Se la playlist non esiste, la creo e aggiungo la traccia
-            createdPlaylist = createPlaylist(playlistName)
-        # Adesso sono certo che la playlist generata esista. Posso aggiungere la traccia
-        self.updateStatus("Playlist generata: " + playlistName)
-
-        for track in tracks:
-            if not is_generating:
-                return
-            # Della playlist creata mi interessa createdPlaylist["id"] per poter aggiungere nuove tracce
-            if not checkPlaylistItems(createdPlaylist["id"], track.id):
-                addItemsToPlaylist(createdPlaylist["id"], track.uri)
-        self.updateStatus("Playlist aggiornata con successo!")
-    
-    # Funzione per aggiornare il messaggio di stato
-    def updateStatus(self, message):
-        self.statusLabel.config(text=message)
-        window.update()
-    
-    def stopPlaylistGeneration(self):
-        global is_generating
-        is_generating = False
-        self.updateStatus("Generazione della playlist interrotta.")
-
-
-config = configparser.RawConfigParser()
-config.read('properties.properties')
-
-CLIENT_ID = config.get("Authentication","CLIENT_ID")
-CLIENT_SECRET = config.get("Authentication","CLIENT_SECRET")
-REDIRECT_URI = config.get("Authentication","REDIRECT_URI")
-
-#Variabili globali
-is_generating = False  # Variabile per tenere traccia dello stato di generazione della playlist
-sp = None  # Variabile globale per l'oggetto SpotifyOAuth
-
-# multiple scopes
-# playlist-modify-public -> Necessario per interagire con le playlist
-oAuthscope = "user-read-email,user-read-private,user-library-read,user-read-playback-state,user-modify-playback-state,user-read-currently-playing, playlist-modify-public, playlist-modify-private"
-
-
 def apri_finestra_principale():
     # Crea la finestra principale
     FinestraPrincipale(window)
@@ -307,36 +309,37 @@ def autentica():
         sp = spotipy.Spotify(auth=token_info['access_token'])
         apri_finestra_principale()
 
-# Autenticato come
-window = ThemedTk(theme="yaru")  # Utilizza il tema 'yaru' per un aspetto simile a Material Design
-window.title("Generatore di Playlist Spotify")
-window.geometry("400x200")  # Imposta le dimensioni della finestra
-#window.resizable(False, False)  # Impedisce all'utente di modificare le dimensioni della finestra
+# Codice principale
+if __name__ == "__main__":
+    window = ThemedTk(theme="yaru")  # Utilizza il tema 'yaru' per un aspetto simile a Material Design
+    window.title("Generatore di Playlist Spotify")
+    window.geometry("400x200")  # Imposta le dimensioni della finestra
+    #window.resizable(False, False)  # Impedisce all'utente di modificare le dimensioni della finestra
 
-# Crea la finestra di autenticazione
-# Crea un Frame per un layout migliore
-mainFrame = ttk.Frame(window, padding="10")
-mainFrame.pack(fill=tk.BOTH, expand=True)
-# Assicurati che le colonne si espandano adeguatamente
-mainFrame.grid_columnconfigure(0, weight=1)
-mainFrame.grid_columnconfigure(1, weight=1)
+    # Crea la finestra di autenticazione
+    # Crea un Frame per un layout migliore
+    mainFrame = ttk.Frame(window, padding="10")
+    mainFrame.pack(fill=tk.BOTH, expand=True)
+    # Assicurati che le colonne si espandano adeguatamente
+    mainFrame.grid_columnconfigure(0, weight=1)
+    mainFrame.grid_columnconfigure(1, weight=1)
 
-# Configura le righe per espandersi, permettendo il centramento verticale
-mainFrame.grid_rowconfigure(0, weight=1)
-mainFrame.grid_rowconfigure(1, weight=1)
-mainFrame.grid_rowconfigure(2, weight=1)
+    # Configura le righe per espandersi, permettendo il centramento verticale
+    mainFrame.grid_rowconfigure(0, weight=1)
+    mainFrame.grid_rowconfigure(1, weight=1)
+    mainFrame.grid_rowconfigure(2, weight=1)
 
-# Aggiungi una casella di testo per l'artista
-artistLabel = ttk.Label(mainFrame, text="Clicca per autenticarti con Spotify")
-# Centra l'etichetta orizzontalmente e verticalmente
-artistLabel.grid(row=0, column=0, columnspan=2, pady=10)
+    # Aggiungi una casella di testo per l'artista
+    artistLabel = ttk.Label(mainFrame, text="Clicca per autenticarti con Spotify")
+    # Centra l'etichetta orizzontalmente e verticalmente
+    artistLabel.grid(row=0, column=0, columnspan=2, pady=10)
 
-# Aggiungi un pulsante per generare la playlist
-generateButton = ttk.Button(mainFrame, text="Login", command=autentica)
-generateButton.config(width=20)  # Imposta la larghezza di generateButton
-# Centra il pulsante orizzontalmente e verticalmente
-generateButton.grid(row=1, column=0, columnspan=2, pady=10)
+    # Aggiungi un pulsante per generare la playlist
+    generateButton = ttk.Button(mainFrame, text="Login", command=autentica)
+    generateButton.config(width=20)  # Imposta la larghezza di generateButton
+    # Centra il pulsante orizzontalmente e verticalmente
+    generateButton.grid(row=1, column=0, columnspan=2, pady=10)
 
-mainFrame.mainloop()
+    mainFrame.mainloop()
 
 
